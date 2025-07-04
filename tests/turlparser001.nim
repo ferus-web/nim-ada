e816be6c0084dbb04909d0c3b2ff01bb9c85ca47
Trayambak Rai <xtrayambak@disroot.org>
2025-07-04 22:18:01 +0530

    add: tests: more cases

--- tests/turlparser001.nim
+++ tests/turlparser001.nim
@@ -12,8 +12,30 @@ suite "basic url parsing tests":
     check(url.scheme == "https:")
     check(url.username.isNone)
     check(url.password.isNone)
+    check(url.schemeType == SchemeType.HTTPS)
 
   test "URL with port":
     let url = parseURL("https://github.com:8090")
 
     check(url.port.isSome and url.port.get() == 8090)
+    check(url.schemeType == SchemeType.HTTPS)
+
+  test "misc":
+    let
+      a = parseURL("http://something.lol/info#about-me")
+      b = parseURL("wss://supersecurechat.xyz")
+      c = parseURL("file:///home/buddy/Documents/Savings.xlsx")
+
+    check(a.schemeType == SchemeType.HTTP)
+    check(b.schemeType == SchemeType.WSS)
+    check(c.schemeType == SchemeType.File)
+
+    check(a.fragment.get() == "#about-me")
+
+    check(a != b)
+    check(b != c)
+    check(a == a)
+    check(b == b)
+    check(c == c)
+
+    check(c.pathname == "/home/buddy/Documents/Savings.xlsx")
